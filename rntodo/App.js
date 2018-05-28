de5affdaa9bc4ce4c52fbd7eec39d681aff1cc62
Abdullah Tariq <abdullahtariq91@gmail.com>
2018-05-29 03:27:43 +0500

    Added styles and states

--- rntodo/App.js
+++ rntodo/App.js
@@ -9,6 +9,27 @@ class Greeting extends Component {
   }
 }
 
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 10000);
+  }
+
+  render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
+    return (
+      <Text>{display}</Text>
+    );
+  }
+}
+
 export default class LotsOfGreetings extends Component {
   render() {
     let pic = {
@@ -20,6 +41,14 @@ export default class LotsOfGreetings extends Component {
         <Greeting name='Rexxar' number='1'/>
         <Greeting name='Jaina' number='2'/>
         <Greeting name='Valeera' number='3' />
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
       </View>
     );
   }
@@ -31,5 +60,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 });
