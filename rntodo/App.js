adf7f046ac1a4848a3c87eb470cc60b02c244b75
Abdullah Tariq <abdullahtariq91@gmail.com>
2018-05-28 23:14:53 +0500

    Learning React-Native for now :)

--- rntodo/App.js
+++ rntodo/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, AppRegistry, Image } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
     return (
+      <Text>Hello {this.props.name}! You are  {this.props.number}!</Text>
+    );
+  }
+}
+
+export default class LotsOfGreetings extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
       <View style={styles.container}>
-        <Text>Testing!</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Greeting name='Rexxar' number='1'/>
+        <Greeting name='Jaina' number='2'/>
+        <Greeting name='Valeera' number='3' />
       </View>
     );
   }
