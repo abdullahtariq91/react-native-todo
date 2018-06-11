a786a4829861162fd725bdd4b4bd1034ff160a3f
Abdullah Tariq <abdullahtariq91@gmail.com>
2018-06-11 22:43:58 +0500

    Starting to-do work

--- rntodo/App.js
+++ rntodo/App.js
@@ -1,79 +1,12 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, AppRegistry, Image, TextInput, Button, Alert, TouchableHighlight } from 'react-native';
+import { StyleSheet, Text, View, AppRegistry } from 'react-native';
 
-class Greeting extends Component {
+export default class StaringWork extends Component {
   render() {
-    return (
-      <Text style={{height: 40, fontSize: 30}}>Hello {this.props.name}! You are cute!</Text>
-    );
-  }
-}
-
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {isShowingText: true};
-
-    // Toggle the state every second
-    setInterval(() => {
-      this.setState(previousState => {
-        return { isShowingText: !previousState.isShowingText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.isShowingText ? this.props.text : ' ';
-    return (
-      <Text>{display}</Text>
-    );
-  }
-}
-
-class PizzaTranslator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
-
-  render() {
-    return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40, fontSize: 30}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
-      </View>
-    );
-  }
-}
-
-export default class LotsOfGreetings extends Component {
-  render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
       <View style={styles.container}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}}>
-         <Greeting name='Abdullah' number='1'></Greeting>
-        </View>
-        <View style={{flex: 1, width: 200, backgroundColor: 'skyblue'}}>
-          <PizzaTranslator></PizzaTranslator>
-        </View>
         <View style={{flex:1, backgroundColor: 'steelblue'}}>
-          <TouchableHighlight 
-            style={styles.button}
-            onPress={() => {
-              Alert.alert('You tapped the button!');
-            }}
-          >
-            <Text>Press Me!</Text>
-          </TouchableHighlight>
+          <Text>Starting Work</Text>
         </View>
       </View>
     );
@@ -87,20 +20,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-  backgroundbox: {
-    backgroundColor: 'steelblue'
-  },
-  button: {
-    alignItems: 'center',
-    backgroundColor: '#DDDDDD',
-    padding: 10
-  }
 });
