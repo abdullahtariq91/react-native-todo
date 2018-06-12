ffeb9d44723d727dfe7c9ff27556488aee376d9f
Abdullah Tariq <abdullahtariq91@gmail.com>
2018-06-12 18:03:04 +0500

    Added Swiper

--- rntodo/App.js
+++ rntodo/App.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, AppRegistry } from 'react-native';
+import Swiper from 'react-native-swiper';
 
-export default class StaringWork extends Component {
-  render() {
+export default class SwiperClass extends Component {
+  render(){
     return (
-      <View style={styles.container}>
-        <View style={{flex:1, backgroundColor: 'steelblue'}}>
-          <Text>Starting Work</Text>
+      <Swiper style={styles.wrapper} showsButtons={true}>
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
         </View>
-      </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
     );
   }
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
+  wrapper: {
   },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
 });
